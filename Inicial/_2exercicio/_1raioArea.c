9f6651b8bf46a77da811467cff569bcacd6f4c74
Adrian de Souza <gilmaradrian127@gmail.com>
2022-03-22 21:40:03 -0300

    Limpando código

--- Inicial/_2exercicio/_1raioArea.c
+++ Inicial/_2exercicio/_1raioArea.c
@@ -16,12 +16,9 @@ int main(int argc, char** argv)
 	printf("Olá, por favor, digite o RAIO do círculo a fim de obter sua área: ");
 	scanf("%f", &raio);
 	
-	//area = pi*(raio*raio);
 	area = pi*pow(raio, 2);
 	
 	printf("%.2f", area);
-		
-	//descobrir função para elevar a potência no C
 
 	return 0;
 }
