a68df6607a3ddb5a9c9a570961bdda478e4ff444
Adrian de Souza <gilmaradrian127@gmail.com>
2022-03-22 21:44:22 -0300

    Resolvendo bugs de exibição

--- Inicial/_2exercicio/_3salarioLiquido.c
+++ Inicial/_2exercicio/_3salarioLiquido.c
@@ -5,37 +5,36 @@ int main(int argc, char** argv)
 {
 	//setando idioma pt-br
 	setlocale(LC_ALL, "Portuguese");
-	
-	//Ex. l.2.1 - Questão 15
-	//Faça um Programa que pergunte o salário, horas trabalhas e mostre o total do salário ao mês
-	
-	//declarando variáveis
-	float salHora, tempoTrabalho, salBruto, salLiquido;
-	
-	//declarando variáveis dos impostos e valor sindical
-	float irpf = 0.11, inss = 0.08, sind = 0.05;
-	
-	
-	//solicitação e entrada de dados
-	printf("Digite quanto você recebe por hora: ");
-	scanf("%f", &salHora);
-	
-	printf("Digite quantas horas você trabalhou esse mês: ");
-	scanf("%f", &tempoTrabalho);
-	
-	//realizando operações, atribuindo valores e mostrando resultado
-	salBruto = salHora*tempoTrabalho;
-	printf("\n+ Salário bruto: R$ %.2f\n", salBruto);
 
-	printf("- Pago ao IRPF: R$ %.2f\n", salBruto*irpf);
-		
-	printf("- Pago ao INSS: R$ %.2f\n", salBruto*inss);
+	//Ex. l.2.1 - QuestÃ£o 15
+	//FaÃ§a um Programa que pergunte o salÃ¡rio, horas trabalhas e mostre o total do salÃ¡rio ao mÃªs
 	
-	printf("- Pago ao Sindicato: R$ %.2f\n", salBruto*sind);
-	
-	salLiquido = salBruto - salBruto*inss - salBruto*irpf - salBruto*sind;
-	
-	printf("\n= Salário líquido: %.2f\n", salLiquido);
-	
+	//declarando variÃ¡veis
+	float salHora, tempoTrabalho, salBruto, salLiquido;
+
+	//declarando variÃ¡veis dos impostos e valor sindical
+	float irpf = 0.11, inss = 0.08, sind = 0.05;
+
+	//solicitaÃ§Ã£o e entrada de dados
+	printf("Digite quanto vocÃª recebe por hora: ");
+	scanf("%f", &salHora);
+
+	printf("Digite quantas horas vocÃª trabalhou esse mÃªs: ");
+	scanf("%f", &tempoTrabalho);
+
+	//realizando operaÃ§Ãµes, atribuindo valores e mostrando resultado
+	salBruto = salHora * tempoTrabalho;
+	printf("\n+ SalÃ¡rio bruto: R$ %.2f\n", salBruto);
+
+	printf("- Pago ao IRPF: R$ %.2f\n", salBruto * irpf);
+
+	printf("- Pago ao INSS: R$ %.2f\n", salBruto * inss);
+
+	printf("- Pago ao Sindicato: R$ %.2f\n", salBruto * sind);
+
+	salLiquido = salBruto - salBruto * inss - salBruto * irpf - salBruto * sind;
+
+	printf("\n= SalÃ¡rio lÃ­quido: %.2f\n", salLiquido);
+
 	return 0;
 }
