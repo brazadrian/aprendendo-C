83ebb5c8485ef8b8ba3ea7f0841a1aef926270f6
Adrian de Souza <gilmaradrian127@gmail.com>
2022-04-03 13:50:57 -0300

    Ex.: repetição com login e senha diferentes.

---
+++ Inicial/_3estruturasRepeticao/ex2.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <ctype.h>
+
+// Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
+
+void ex2(){
+
+    // declarando variáveis, senha e nome como caractéres
+    char nome, senha;
+
+    printf("Digite seu nome de usuário: ");
+    scanf(" %c", &nome);
+
+    printf("Digite sua senhas: ");
+    scanf(" %c", &senha);
+
+    // Repetir e solicitar entrada, enquanto nome e senha forem iguais
+    while (nome == senha)
+    {
+        printf("Favor digite seu nome de usuário e senha diferentes:\n\n");
+        printf("Digite seu nome de usuário: ");
+        scanf(" %c", &nome);
+
+        printf("Digite sua senhas: ");
+        scanf(" %c", &senha);
+    }
+    printf("\nParabéns! Você digitou a senha corretamente.");
+}
