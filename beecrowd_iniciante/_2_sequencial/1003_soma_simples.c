0430be8a90b80530091aa748791781facd050b7d
Adrian de Souza <gilmaradrian127@gmail.com>
2022-06-10 21:44:46 -0300

    Soma simples

---
+++ beecrowd_iniciante/_2_sequencial/1003_soma_simples.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // declarando variáveis 
+    int a, b, soma;
+
+    // entrada de dados
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    // processamento
+    soma = a + b;
+
+    // saída
+    printf("SOMA = %d\n", soma);
+
+    return 0;
+}
