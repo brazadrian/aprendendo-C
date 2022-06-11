41d8839752849e5f8124890c8fab51993a34c99d
Adrian de Souza <gilmaradrian127@gmail.com>
2022-06-10 21:38:04 -0300

    Soma

---
+++ beecrowd_iniciante/_2_sequencial/1001_soma.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    // declarando variáveis
+    int A, B, X;
+
+    // entrada
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    // processamento
+    X = A + B;
+
+    // saida
+    printf("X = %d\n", X);
+
+    return 0;
+}
