e62d8769eec848e28d3c11727fe45349333f3a7d
Adrian de Souza <gilmaradrian127@gmail.com>
2022-06-10 21:45:49 -0300

    Produto simples

---
+++ beecrowd_iniciante/_2_sequencial/1004_produto_simples.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // declarando variáveis
+    int n1, n2, prod;
+
+    // entrada de dados
+    scanf("%d", &n1);
+    scanf("%d", &n2);
+
+    // processamento
+    prod = n1 * n2;
+
+    // saída
+    printf("PROD = %d\n", prod);
+
+    return 0;
+}
