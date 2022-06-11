671efe5d06247e758aa8a5afa5e6290828e96d5d
Adrian de Souza <gilmaradrian127@gmail.com>
2022-06-11 00:23:47 -0300

    Tempo necessário para percorrer distância X

---
+++ beecrowd_iniciante/_2_sequencial/1016_distancia.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    // declaração de variáveis
+    int distancia, tempo;
+
+    // entrada de dado
+    scanf("%d", &distancia);
+
+    // processamento
+    tempo = distancia * 2;
+
+    // saída
+    printf("%d minutos\n", tempo);
+
+    return 0;
+}
