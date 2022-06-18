63e7bdda829d35f7f22c01956236e6f474e266e9
Adrian de Souza <gilmaradrian127@gmail.com>
2022-06-18 06:17:16 -0300

    Alunos aprovados
    Incompleto

---
+++ Inicial/_6vetor-e-looping/ex6.h
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+Faça um Programa que peça as quatro notas de 10 alunos
+calcule e armazene num vetor a média de cada aluno
+imprima o número de alunos com média maior ou igual a 7.0.
+ */
+
+#define TAMVETNOTA 3
+#define MEDIANOTA 7
+
+ex6()
+{
+    int nota-aluno;
+
+    for (int cont = 0; cont < TAMVETNOTA; cont++)
+    {
+        
+    }
+    
+
+    printf("Digite a %dª nota: ");
+}
