11d0c419a34e051ac3ac8deea79438c44f0e5297
Adrian de Souza <gilmaradrian127@gmail.com>
2022-06-28 22:06:10 -0300

    Cabecalho para manipulacao de vetor e matriz

---
+++ Inicial/_8final-unidade/manipula_vetor.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/* 
+void imprime_vetor(int *V, int tamV)
+{
+    for (int i = 0; i < tamV; i++)
+    {
+        printf("%d\n", V[i]);
+    }
+}
+ */
+
+/*
+void ordena_vetor(int *v, int tamV){
+    int ram = 0;
+    for(int i = 0; i < tamV; i++){
+        for(int j = 0; j < tamV; j++){
+            if(v[i] > v[j]){
+                ram = v[i];
+                v[i] = v[j];
+                v[j] = ram;
+            }
+        }
+        printf("%d", v[i]);
+    }
+}
+ */
