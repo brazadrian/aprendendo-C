95e5ddf1cd4744d0efa5934f40e8789d2e604cac
brazadrian <gilmaradrian127@gmail.com>
2022-07-11 09:55:37 -0300

    atFinal_e_crud_partimentado

---
+++ Intermediaria/5_at_final_pedidos/menu.h
@@ -0,0 +1,23 @@
+
+int mn_cardapio(int *o) // o -> opcao
+{
+    printf("Olá, seja bem vindo!\n");
+    printf("----- CARDÁPIO -----\n");
+    printf("1 - Comidas\n");
+    printf("2 - Bebidas\n");
+    printf("0 - Finalizar pedido\n");
+    printf("--------------------\n");
+    printf("Faça seu pedido, digite sua opção: \n");
+    scanf("%d", &o);
+
+    return o;
+}
+
+mn_comidas()
+{
+    printf("----------\n");
+    printf("1 - Comidas\n");
+    printf("2 - Bebidas\n");
+    printf("0 - Finalizar pedido\n");
+    printf("--------------------\n");
+}
