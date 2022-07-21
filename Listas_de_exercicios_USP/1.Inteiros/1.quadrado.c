1441f62d6831e980d3bc454f20529fe63c4b77be
brazadrian <gilmaradrian127@gmail.com>
2022-07-20 23:02:29 -0300

    ajustes nos comentarios

--- Listas_de_exercicios_USP/1.Inteiros/1.quadrado.c
+++ Listas_de_exercicios_USP/1.Inteiros/1.quadrado.c
@@ -4,14 +4,23 @@
 
 int main()
 {
-    int num = !0, pow;
+    /* Declarando variáveis */
+    int num = !0, /* para a entrada do usuário */
+        pow;      /* para o quadrado da entrada */
+
+    /* Laço de repetição para cumprir com a sequência */
     do
     {
         printf("\n\nPara descobrir o quadrado de certo valor");
         printf("\nPara sair digite \'0\'");
+
+        /* Entrada de dados */
         printf("\nDigite um número: ");
         scanf("%d", &num);
+
+        /* Processamento de dados */
         pow = num * num;
+        /* Saída de dados */
         printf("\nO quadrado de %d é: %d", num, pow);
     } while (num != 0);
 
--- Listas_de_exercicios_USP/1.Inteiros/2_soma_fatorial.c
+++ Listas_de_exercicios_USP/1.Inteiros/2_soma_fatorial.c
@@ -4,16 +4,23 @@
 
 int main()
 {
-    int n, i, x;
+    /* Declaração de variáveis */
+    int n, /* para a entrada do usuário */
+        i, /* contador para laço de repetição */
+        x; /* para o resultado da soma geral */
 
+    /* Entrada de dados */
     printf("\nCalcular a soma dos n primeiros números inteiros positivos");
     printf("\nDigite o valor de n: ");
     scanf("%d", &n);
 
+    /* Processamento de dados */
     for (i = n; i > 0; i--)
     {
         x += i;
     }
+
+    /* Saída de dados */
     printf("\nA soma dos n primeiros números é igual a: %d\n", x);
 
     return 0;
