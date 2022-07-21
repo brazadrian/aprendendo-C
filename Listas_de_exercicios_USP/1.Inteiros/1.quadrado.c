029a8a8548d5d7bb5da29b001ca9a00105280617
brazadrian <gilmaradrian127@gmail.com>
2022-07-20 22:25:53 -0300

    resolucao de exercicio para quadrado de entrada

---
+++ Listas_de_exercicios_USP/1.Inteiros/1.quadrado.c
@@ -0,0 +1,19 @@
+// Dada uma seqüência de números inteiros não-nulos, seguida por 0, imprimir seus quadrados.
+
+#include <stdio.h>
+
+int main()
+{
+    int num = !0, pow;
+    do
+    {
+        printf("\nPara descobrir o quadrado de certo valor");
+        printf("\nPara sair digite \'0\'");
+        printf("\nDigite um número: ");
+        scanf("%d", &num);
+        pow = num * num;
+        printf("O quadrado do número digitado é: %d", pow);
+    } while (num != 0);
+
+    return 0;
+}
